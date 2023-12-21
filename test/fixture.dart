e1888aecd178c17d5f97b258e929b854f3117cf0
miyazawa <13tmtmtommy@gmail.com>
2023-12-21 10:25:02 +0900

    調整

--- test/fixture.dart
+++ test/fixture.dart
@@ -2,9 +2,5 @@ import 'dart:io';
 
 // テストデータをファイルから読み込み
 String fixture(String name) {
-  var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
-  }
-  return File('$dir/test/fixtures/$name').readAsStringSync();
+  return File('test/stub/$name').readAsStringSync();
 }
--- test/movies_search_test.dart
+++ test/movies_search_test.dart
@@ -13,13 +13,14 @@ void main() {
     late DioAdapter dioAdapter;
     late TmdbApiClient apiClient;
     // 各テストケースが実行される前に行う初期設定
-    setUp(() async {
+    setUp(() {
       dio = Dio();
       dioAdapter = DioAdapter(dio: dio);
       apiClient = TmdbApiClient(dio);
     });
     // 映画データを返すことをテスト
-    test('fetchSearchMOviesItems moviesSearchData', () async {
+    test('fetchSearchMoviesItems returns MoviesSearchData for valid response',
+        () async {
       // スタブデータ
       final mockResponse = fixture('movies_search.json');
       // 呼び出されたときに、成功したレスポンスを返すように設定
@@ -36,40 +37,22 @@ void main() {
       final result = await apiClient.fetchSearchMoviesItems('ゴジラ');
       // レスポンスが MoviesSearchData型であることを検証
       expect(result, isA<MoviesSearchData>());
-      // レスポンスデータの検証
-      expect(result.results, isNotEmpty);
-      for (var movie in result.results) {
-        expect(movie.id, isNotNull);
-        expect(movie.id, isA<int>());
-        expect(movie.title, isNotNull);
-        expect(movie.title, isA<String>());
-        expect(movie.posterPath, isNotNull);
-        expect(movie.posterPath, isA<String>());
-      }
     });
 
-    // エラーケース
-    test('fetchSearchMOviesItems 404 Error', () async {
+    test('fetchSearchMoviesItems throws error for 404 response', () async {
       // 呼び出されたときに、成功したレスポンスを返すように設定
       dioAdapter.onGet(
         'search/movie',
         (server) => server.reply(404, {'message': 'Not Found'}),
         queryParameters: {
-          'query': 'ああああああああああ',
+          'query': 'モニラ',
           'language': 'ja-JA',
           'page': 1,
         },
       );
       // APIメソッドがエラーを投げることを検証
-      try {
-        await apiClient.fetchSearchMoviesItems('ああああああああああ');
-      } catch (e) {
-        if (e is DioException) {
-          print('Caught DioError: ${e.response?.data}');
-        } else {
-          rethrow;
-        }
-      }
+      expect(() async => await apiClient.fetchSearchMoviesItems('モニラ'),
+          throwsA(isA<DioException>()));
     });
   });
 }
